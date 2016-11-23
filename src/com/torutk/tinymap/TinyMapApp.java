b918a80d162647ee429c7a629c8c652267774599
TAKAHASHI,Toru <torutk@gmail.com>
2016-11-24 04:23:38 +0900

    JavaFX FXMLアプリケーション雛形から生成された最初の状態

---
+++ src/com/torutk/tinymap/TinyMapApp.java
@@ -0,0 +1,35 @@
+/*
+ * © 2016 Toru Takahashi
+ */
+package com.torutk.tinymap;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Toru Takahashi
+ */
+public class TinyMapApp extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("TinyMapView.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
